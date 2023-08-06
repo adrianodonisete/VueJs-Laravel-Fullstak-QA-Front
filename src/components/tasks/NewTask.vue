6a35dda334981549e96d3632d8193c8a41734d63
Adriano Machado <adrianodonisete@gmail.com>
2023-08-06 11:08:03 -0300

    add feature NewTask

---
+++ src/components/tasks/NewTask.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative">
+    <input type="text" class="form-control form-control-lg padding-right-lg"
+      placeholder="+ Add new task. Press enter to save." @keydown.enter="addNewTask" />
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const newTask = reactive({
+  name: '',
+  is_completed: false,
+});
+
+const emit = defineEmits(['added']);
+
+const addNewTask = async (event) =>
+{
+  const descriptionTask = event.target.value.trim();
+  if (descriptionTask)
+  {
+    newTask.name = descriptionTask;
+    event.target.value = '';
+
+    await emit('added', newTask);
+  }
+};
+</script>
--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -4,19 +4,22 @@
       <div class="row">
         <div class="col-md-8 offset-md-2">
           <!-- Add new Task -->
-          <div class="relative">
-            <input type="text" class="form-control form-control-lg padding-right-lg"
-              placeholder="+ Add new task. Press enter to save." />
-          </div>
+          <NewTask @added="handledAddedTask" />
 
           <!-- List of tasks uncompleted -->
           <Tasks :tasks="uncompletedTasks" />
 
           <!-- show toogle botton -->
+          <div class="text-center my-3" v-show="showToggleCompletedBtn"
+            @click="$event => showCompletedTasks = !showCompletedTasks">
+            <button class="btn btn-sm btn-secondary">
+              <span v-if="!showCompletedTasks">Show completed</span>
+              <span v-else>Hide completed</span>
+            </button>
+          </div>
 
           <!-- List of tasks completed -->
-          <Tasks :tasks="completedTasks" />
-
+          <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks" />
 
         </div>
       </div>
@@ -26,8 +29,9 @@
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
-import { allTasks } from '../http/task-api';
+import { allTasks, createTask } from '../http/task-api';
 import Tasks from '../components/tasks/Tasks.vue';
+import NewTask from '../components/tasks/NewTask.vue';
 
 const tasks = ref([]);
 
@@ -37,7 +41,23 @@ onMounted(async () =>
   tasks.value = data.data;
 });
 
-const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed));
+const uncompletedTasks = computed(() => tasks.value.filter(task => !task?.is_completed));
 
-const completedTasks = computed(() => tasks.value.filter(task => task.is_completed));
+const completedTasks = computed(() => tasks.value.filter(task => task?.is_completed));
+
+const showToggleCompletedBtn = computed(
+  () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+);
+
+const completedTasksIsVisible = computed(
+  () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
+);
+
+const showCompletedTasks = ref(false);
+
+const handledAddedTask = async (newTask) =>
+{
+  const { data: addedTask } = await createTask(newTask);
+  tasks.value.unshift(addedTask.data);
+}
 </script>
